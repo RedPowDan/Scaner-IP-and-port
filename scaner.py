1a6155e3acd527cba411c8039e461ff86b7fba31
Danil <11051969dahul@mail.ru>
2021-10-11 19:22:36 +0500

    Допилил

--- scaner.py
+++ scaner.py
@@ -1,12 +1,14 @@
 import socket
 import sys
 import threading
+import time
 from datetime import datetime
 
 
 class Scaner:
-    MAX_SECONDS_TIMEOUT_SOCKET = 2
+    MAX_SECONDS_TIMEOUT_SOCKET = 0.5
     MAX_RANGE_PORT = 65535
+    _list_of_messages = []
 
     def scan_ip_range_and_port_range(self, begin_ip, end_ip, begin_port, end_port):
         last_node_in_begin_ip = int(self.get_last_node_in_ip(begin_ip))
@@ -19,6 +21,8 @@ class Scaner:
             ip = '.'.join(parsed_begin_ip[:3]) + '.' + str(last_node)
             self.scan_ip_in_range_port(ip=ip, begin_port=begin_port, end_port=end_port)
 
+        self.print_message_list()
+
     def scan_ip_in_range_port(self, ip, begin_port, end_port):
         if end_port > self.MAX_RANGE_PORT:
             end_port = self.MAX_RANGE_PORT
@@ -30,24 +34,32 @@ class Scaner:
             task = threading.Thread(target=self.scan_thread, args=(ip, port))
             task.start()
 
+        self.print_message_list()
+
     def scan_thread(self, ip, port):
         status, out_info_about_port = self.scan_ip_port(ip, port)
         if status == 1:
-            print(ip + ':' + str(port), ' its open.' + 'info -> ' + out_info_about_port)
+            self._list_of_messages.append(ip + ':' + str(port) + ' its open.' + 'info -> ' + out_info_about_port)
 
     def scan_ip_port(self, ip, port) -> [int, str]:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(self.MAX_SECONDS_TIMEOUT_SOCKET)
         try:
             connect = sock.connect((ip, port))
-            bann = sock.recv(2048)
-            out_info_about_port = bann.decode('utf-8').strip()
+            try:
+                bytes = sock.recv(2048)
+                out_info_about_port = bytes.decode('utf-8').strip()
+            except:
+                out_info_about_port = "Not found info"
             sock.close()
             return 1, out_info_about_port
         except:
             pass
 
         return 0, None
+
+    def print_message_list(self):
+        [print(message) for message in self._list_of_messages]
 
     def get_last_node_in_ip(self, ip):
         return self.parse_ip(ip)[3]
